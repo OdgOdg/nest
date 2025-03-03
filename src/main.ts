32f4f453166a9d5d2dbe268ea4ee5b820de3771a
Yeongjun Joo <wndudwns6824@naver.com>
2025-03-03 17:50:26 +0900

    chore : 스웨거 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('API 문서')
+    .setDescription('NestJS Swagger 예제 API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT 인증 추가 (필요 시)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // Swagger URL: http://localhost:8000/api-docs
+
+  await app.listen(8000);
 }
 bootstrap();
