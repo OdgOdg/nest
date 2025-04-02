a756b4364fa7feaa2c3aa8f957aabb88361ffbcb
Yeongjun Joo <wndudwns6824@naver.com>
2025-04-02 19:09:32 +0900

    refactor : 파일 위치 수정

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from 'src/global/redis/redis.module';
+import { EventModule } from './module/events/event.module';
+import { UserModule } from './module/user/user.module';
+import { AuthModule } from './module/auth/auth.module';
+import { ChatModule } from './module/chat/chat.module';
+import { FriendsModule } from './module/friends/friends.module';
+
+@Module({
+  imports: [
+    // ConfigModule로 환경 변수 로드
+    ConfigModule.forRoot({
+      isGlobal: true, // 모든 모듈에서 환경 변수를 사용할 수 있게 설정
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    EventModule,
+    UserModule,
+    AuthModule,
+    ChatModule,
+    FriendsModule,
+    RedisModule,
+  ],
+})
+export class AppModule {}
